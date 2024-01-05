c7f47cca2cdf9c59fb29a48806065d0abc4dfc3d
wilsonckm <wilsonckmchan@gmail.com>
2024-01-04 21:20:40 -0500

    Initial Commit

---
+++ exerciselibrary/exerciselibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  exerciselibraryApp.swift
+//  exerciselibrary
+//
+//  Created by Wilson Chan on 1/4/24.
+//
+
+import SwiftUI
+
+@main
+struct exerciselibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
