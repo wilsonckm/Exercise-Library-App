cf99a3b6ab63b1ebf4fed3234bd4af86648e959c
wilsonckm <wilsonckmchan@gmail.com>
2024-01-06 22:37:54 -0500

    -Add error handing in apiCall -Rename apiCall to ExerciseSearch -Refactor and move api call to DataService

--- exerciselibrary/ContentView.swift
+++ exerciselibrary/ContentView.swift
@@ -8,59 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var service = DataService()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Text("")
         }
         .padding()
-        .onAppear() {
-            Task {
-                await apiCall()
-            }
-        }
-    }
-    
-    func apiCall() async -> [Exercise] {
-        // Gets URL + Optional binding to handle URL
-        let apiKey = Bundle.main.infoDictionary?["API_KEY"] as? String
-        
-        guard apiKey != nil else {
-            return [Exercise]()
+        .task {
+            await service.exerciseSearch()
         }
         
-        
-        if let url = URL(string: "https://exercisedb.p.rapidapi.com/exercises?limit=10") {
-            
-            //URL request with multiple headers: use setValue rather than addValue! or use .allHTTPHeaderFields
-            var request = URLRequest(url: url)
-            request.httpMethod = "GET"
-            request.allHTTPHeaderFields = [
-                "X-RapidAPI-Key": "\(apiKey!)",
-                   "X-RapidAPI-Host": "exercisedb.p.rapidapi.com"
-               ]
-
-            do {
-                let (data, response) = try await URLSession.shared.data(for: request)
-                let decoder = JSONDecoder()
-                
-                do {
-                    let result = try decoder.decode([Exercise].self, from: data)
-        
-                }
-                catch {
-                    print(error)
-                }
-            }
-            catch {
-                print(error)
-            }
-        }
-        return [Exercise]()
     }
 }
+
 
 #Preview {
     ContentView()
---
+++ exerciselibrary/DataService.swift
@@ -0,0 +1,64 @@
+//
+//  DataService.swift
+//  exerciselibrary
+//
+//  Created by Wilson Chan on 1/6/24.
+//
+
+import Foundation
+
+struct DataService {
+
+    func exerciseSearch() async -> [Exercise] {
+        // Gets API key as an optional string from the infoplist
+        let apiKey = Bundle.main.infoDictionary?["API_KEY"] as? String
+        
+        //Checks if API key exists. Guard is used here to handle missing/invalid api key.
+        guard apiKey != nil else {
+            return [Exercise]()
+        }
+        
+        // Gets URL + Optional binding to handle URL
+        if let url = URL(string: "https://exercisedb.p.rapidapi.com/exercises?limit=10") {
+            
+            //URL request with multiple headers: use setValue rather than addValue! or use .allHTTPHeaderFields
+            var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+            request.allHTTPHeaderFields = [
+                "X-RapidAPI-Key": "\(apiKey!)",
+                "X-RapidAPI-Host": "exercisedb.p.rapidapi.com"
+            ]
+            do {
+                let (data, response) = try await URLSession.shared.data(for: request)
+                
+                if let httpResponse = response as? HTTPURLResponse {
+                    //Checks of status code is between 200 and 299 (Successful response)
+                    if (200..<300).contains(httpResponse.statusCode) {
+                        print("The status code is: \(httpResponse.statusCode)")
+                        //Decode the JSON data received from the API
+                        let decoder = JSONDecoder()
+                        do {
+                            let result = try decoder.decode([Exercise].self, from: data)
+                        }
+                        catch {
+                            //catches possible decoding error.
+                            print("Decoding error: \(error)")
+                        }
+                    } else {
+                        // Handle non-successful HTTP status codes
+                        print("HTTP status code: \(httpResponse.statusCode)")
+                    }
+                } else {
+                    //Handle unexpected non-HTTP response
+                    print("Unexpected response type")
+                }
+            } catch {
+                //Handle URLSession errors
+                print("URLSession error: \(error)")
+            }
+        }
+        return [Exercise]()
+    }
+    
+}
+
